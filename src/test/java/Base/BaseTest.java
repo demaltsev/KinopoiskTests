4e13afb31ddd366ab72f900651a68290c8af9e06
demaltcev <demaltcev@yandex.ru>
2023-10-15 10:57:10 +0300

    Initial commit

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,62 @@
+package Base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    protected static WebDriver driver;
+    private static final int TIME_OUT = 15;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "\\Users\\Deni\\IdeaProjects\\KinopoiskTests\\src\\main\\resources\\chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(TIME_OUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(TIME_OUT, TimeUnit.SECONDS);
+    }
+
+    /*@AfterEach
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }*/
+
+    public static void switchTo(int window) {
+        Set<String> tabs = driver.getWindowHandles();
+        ArrayList<String> tabsArray = new ArrayList<>(tabs);
+        driver.switchTo().window(tabsArray.get(window));
+    }
+
+    public static void switchTo(String pagePath) {
+        Set<String> tabs = driver.getWindowHandles();
+        for (String tab : tabs) {
+            driver.switchTo().window(tab);
+            if (driver.getCurrentUrl().startsWith(pagePath)) {
+                return;
+            }
+        }
+        throw new RuntimeException("Вкладка не найдена");
+    }
+
+
+    public static boolean checkCookiesContains(String key, String value) {
+        return driver.manage().getCookieNamed(key).getValue().contains(value);
+    }
+
+    public static boolean checkCookieEquals(String key, String value) {
+        return driver.manage().getCookieNamed(key).getValue().equals(value);
+    }
+
+
+}
+
